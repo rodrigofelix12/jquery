13cffd26992e027820fefe6d29e7435004c5125b
Rodrigo <c1298299>
2019-02-07 11:25:45 -0200

    Funções que auxiliam os estilos aula 05

--- alura-typer/public/js/main.js
+++ alura-typer/public/js/main.js
@@ -5,6 +5,9 @@ $(function() {
     atualizaTamanhoFrase();
     inicializaContadores();
     inicializaCronometro();
+
+    inicializaMarcadores();
+
     $("#botao-reiniciar").click(reiniciaJogo);
 })
 
@@ -35,7 +38,9 @@ function inicializaCronometro() {
             if (tempoRestante < 1) {
                 campo.attr("disabled", true);
                 clearInterval(cronometroID);
+                
                 $("#botao-reiniciar").attr("disabled", false);
+                campo.toggleClass("campo-desativado");
             }
         }, 1000);
     });    
@@ -51,6 +56,26 @@ function reiniciaJogo() {
         $("#contador-caracteres").text("0");
         $("#tempo-digitacao").text(tempoInicial);
         inicializaCronometro();
+        campo.toggleClass("campo-desativado");
+
+        campo.removeClass("borda-vermelha");
+        campo.removeClass("borda-verde");
     });    
+}
+
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input", function() {
+        var digitado = campo.val();
+        var comparavel = frase.substr(0, digitado.length);
+
+        if (digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
+        }
+    });
 }
 
